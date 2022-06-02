608d3e6695b09657d390eec8b04fdbc5ae83f87d
decaduto <78024779+decaduto@users.noreply.github.com>
2022-06-02 22:15:14 +0200

    added the objcopy trick: now in ath9u executable, in .firmware section there will be stored the effective firmware
    Now need to plan the ramdisk part, we need to operate in address space only. More info in the README.md

--- compile.sh
+++ compile.sh
@@ -1,1 +1,4 @@
-gcc -no-pie -ldialog -lncurses -lusb-1.0  pprTTxv.c -o ath9u
+gcc -c pprTTxv.c
+objcopy --add-section .firmware=ath9u_fw/htc_9271.fw --set-section-flags .firmware=load,readonly pprTTxv.o final.o
+gcc -ldialog -lncurses -lusb-1.0  final.o -o ath9u
+rm *.o
