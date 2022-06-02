830762fcc583945ee5575d5563e3402243f2b446
decaduto <78024779+decaduto@users.noreply.github.com>
2022-06-03 01:53:11 +0200

    updated source, fixed allocation
    - Added more #pragma message for debugging features
    - Started the development of signal.h subsystem
    - fixed the relocation error of init_dialog
    - Started the development of the custom plugin interface, next to do is to create a macro which created  <GENERATE_PLUGIN_CB_N> callback
    - Redefined in memory detail struct
    - updated the dl_iterate_phdr function, this will be used later for segment enumeration
    
    * Need to start the message shell subsystem, will be pretty simple: allocate a unsigned char * buffer with a circular buffer queue, which will be refreshed when the buffer will be filled, it will run in parallel (pthread.h) and may interact with the main program through shared memory mechanism, for handling with events in real time (really small delay).

--- pprTTxv.c
+++ pprTTxv.c
@@ -4,9 +4,10 @@
 		2021-2022 Edoardo Mantovani All Rights Reserved
 */
 
-#ifdef __ELF__
-    #pragma message("The output executable format is an ELF")
+#if defined(__ELF__) && defined(__GNUC__)
+    #pragma message("GCC: The output executable format is an ELF")
 
+#define FUSE_USE_VERSION 30
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,13 +17,19 @@
 #include <pthread.h>
 #include <link.h>
 #include <stdbool.h>
+#include <signal.h>
 #include <dialog.h>
 #include <ncurses.h>
+#include <fuse.h>
 
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <sys/mman.h>
 #include <libusb-1.0/libusb.h>
+
+#pragma message("built-in headers are: elf, signal, dialog, link, pthread")
+#pragma message("external headers are: libusb, ncurses, fuse")
+#pragma message("system dependent headers are: uio, mman")
 
 #ifdef __thread
     #pragma message("__thread only allocation is available!")
@@ -90,6 +97,8 @@ enum{
     	DEV_IS_80211N,
     	DEV_IS_80211AC,
     	DEV_IS_80211AX,
+	DEV_IS_CUSTOM_FIRMWARE,
+	DEV_IS_SPECTRAL_COMPATIBLE,
 };
 
 enum dev_bus{
@@ -122,7 +131,7 @@ static struct ath9u_device_informations{
     	bool           device_is_supported;
 	bool           device_is_plugged;
 	bool 	       device_has_modified_fw;
-    	bool           device_support_legacy_dfu;
+    	bool           device_support_legacy_dfu; /* this will be discovered by reading its USB descriptor */
 }__thread global_device_informations = {
 	.device_has_modified_fw = false,
 
@@ -131,16 +140,39 @@ static struct ath9u_device_informations{
 
 
 static struct global_address_space_info{
-	uint32_t boot_section_address;
-	uint32_t ath9u_ops_section_address;
-	uint32_t compiler_section_address;
-	uint32_t source_section_address;
+	uint32_t boot_segment_vaddr;
+	uint32_t boot_segment_memsz;
+	uint32_t ath9u_ops_segment_vaddr;
+	uint32_t athu9u_ops_segment_memsz;
+	uint32_t compiler_segment_vaddr;
+	uint32_t compiler_segment_memsz;
+	uint32_t source_segment_vaddr;
+	uint32_t source_segment_memsz;
+	uint32_t firmware_segment_vaddr;
+	uint32_t firmware_segment_memsz;
 }__thread __attribute__((aligned(1))) global_address = {
-	.boot_section_address 	   = 0x00,
-	.ath9u_ops_section_address = 0x00,
-	.compiler_section_address  = 0x00,
-	.source_section_address    = 0x00,
+	.boot_segment_vaddr = 0x00,
+	.boot_segment_memsz = 0x00,
+	.ath9u_ops_segment_vaddr = 0x00,
+	.athu9u_ops_segment_memsz = 0x00,
+	.compiler_segment_vaddr = 0x00,
+	.compiler_segment_memsz = 0x00,
+	.source_segment_vaddr = 0x00,
+	.source_segment_memsz = 0x00,
+	.firmware_segment_vaddr = 0x00,
+	.firmware_segment_memsz = 0x00,
 };
+
+#ifndef GENERATE_PLUGIN_CB_N
+	#define GENERATE_PLUGIN_CB_N	3
+#endif
+
+#define GENERATE_PLUGIN_CB(callback_number)	{	\
+	int (*plugin_callback_ ##callback_number)();	\
+}	\
+
+#define INITIALIZE_PLUGIN_CB(callback_number)	{	\
+}
 
 __thread struct{
 	void (*upload_firmware)(struct libusb_device_handle *, const void *, size_t);
@@ -153,6 +185,7 @@ __thread struct{
 	void (*delete_plugin)();
 	void (*manage_plugin_list)();
 	void (*activate_plugin)();
+	#pragma message("plugin_callback number is: %d" GENERATE_PLUGIN_CB_N)
     	int  (*plugin_callback1)();
     	int  (*plugin_callback2)();
     	int  (*plugin_callback3)();
@@ -175,12 +208,16 @@ __thread struct{
 };
 
 __attribute__((optimize("O0"))) int ath9u_discover_address_space(struct dl_phdr_info *info, size_t size, void *data){
-    /* get the ELF header for gathering the e_entry information */
-    Elf64_Ehdr *athu_main_header = (Elf64_Ehdr *)info->dlpi_addr;
-    for(int i = 0; i < info->dlpi_phnum; i++){
-        //if( dlpi_phdr[i].p_flags /* TODO: manipulation of the phdrs */ )
-
-    //}
+	/* get the ELF header for gathering the e_entry information */
+	Elf64_Ehdr *athu_main_header = (Elf64_Ehdr *)info->dlpi_addr;
+	uint32_t entry_point = info->dlpi_addr + athu_main_header->e_entry;
+	for(int i = 0; i < info->dlpi_phnum; i++){
+		printf("segment start: 0x%x segment end: 0x%x segment size: %d segment flag: 0x%x\n",
+			info[i].dlpi_phdr->p_vaddr,
+			info[i].dlpi_phdr->p_vaddr + info[i].dlpi_phdr->p_memsz,
+			info[i].dlpi_phdr->p_memsz,
+			info[i].dlpi_phdr->p_flags
+		);
 	}
 
 }
@@ -197,9 +234,9 @@ __attribute__((__section__(".boot, \"xaw\", @progbits#"))) __attribute__((constr
 	unsigned char ath9u_device_descriptor[ATH9U_DESCRIPTOR_SIZE];
     	memset(ath9u_device_descriptor, 0x00, ATH9U_DESCRIPTOR_SIZE);
 	/* get the ath9u_device */
-		if( libusb_init(NULL) < 0 ){
-			exit(-LIBUSB_INIT_FAIL);
-		}
+	if( libusb_init(NULL) < 0 ){
+		exit(-LIBUSB_INIT_FAIL);
+	}
 	struct libusb_device_handle *ath9u_device = NULL;
     	global_device_informations.device_vendor_id = ath9u_vid;
     	global_device_informations.device_product_id = ath9u_pid;
@@ -232,13 +269,11 @@ __attribute__((__section__(".boot, \"xaw\", @progbits#"))) __attribute__((constr
     	libusb_free_bos_descriptor(ath9u_configuration_descriptor);
 	/* discover the endpoint, which will be used for every USB bulk/interrupt/control exchange */
 	out_endpoint = 0x1;
-	/* init the dialog API */
-	init_dialog(stdin, stdout);
 }
 
-__attribute__((destructor())) void end(void){
+__attribute__((__section__(".end, \"xaw\", @progbits#"))) __attribute__((destructor())) void end(void){
 	end_dialog(); /* terminate the dialog API */
-
+	libusb_exit(NULL);
 }
 
 signed int ddv_send_management_frame(struct libusb_device_handle *ath9u_usb_handler, const void *tx_frame){
@@ -302,7 +337,6 @@ void ddv_upload_fw(struct libusb_device_handle *ath9u_usb_handler, const void *f
 		#define AR9271_FIRMWARE_TEXT  0x903000
 	#endif
 	libusb_control_transfer(ath9u_usb_handler, 0x40 | USB_DIR_OUT, FIRMWARE_DOWNLOAD_COMP, AR9271_FIRMWARE_TEXT >> 8, 0, NULL, 0, USB_MSG_TIMEOUT);
-	libusb_exit(NULL);
 }
 
 
@@ -316,7 +350,7 @@ void ddv_eject_device(struct libusb_device_handle *ath9u_usb){
 
 	/* allocate the BULK URB packet to send to the adapter */
 	unsigned char *ath9u_bulk_cmd = (unsigned char *)calloc(31, sizeof(char));
-
+	memset(ath9u_bulk_cmd, 0x00, sizeof(ath9u_bulk_cmd));
 	ath9u_bulk_cmd[0]  = 0x55;	/* bulk command signature */
 	ath9u_bulk_cmd[1]  = 0x53;	/* bulk command signature */
 	ath9u_bulk_cmd[2]  = 0x42;	/* bulk command signature */
@@ -347,20 +381,23 @@ signed int ddv_reboot_device(struct libusb_device_handle *ath9u_device){
 	#else
     		#define ATH9U_REBOOT_CMD	0xffffffff
 	#endif
-	void *reboot_buf;
+	void *reboot_buf = malloc(sizeof(0xffffffff));
     	memcpy(reboot_buf, (void *)ATH9U_REBOOT_CMD, 4);
     	if( libusb_interrupt_transfer(ath9u_device, out_endpoint, reboot_buf, 4, NULL, USB_MSG_TIMEOUT) < 0 ){
         	exit(-LIBUSB_CONTROL_FAIL);
     	}
+	free(reboot_buf);
 }
 
 long ath9u_get_device_capabilities(void){
 	return( DEV_CAN_TX | DEV_CAN_RX | DEV_CAN_STA_MODE | DEV_CAN_AP_MODE | DEV_CAN_MONITOR_MODE |
-            DEV_CAN_2GHZ | DEV_CAN_INJECT |  DEV_CAN_SUPPORT_WPA | DEV_CAN_SUPPORT_WPA2 | DEV_IS_80211N );
+            DEV_CAN_2GHZ | DEV_CAN_INJECT |  DEV_CAN_SUPPORT_WPA | DEV_CAN_SUPPORT_WPA2 | DEV_IS_80211N | DEV_IS_SPECTRAL_COMPATIBLE);
 
 }
 
 int main(int argc, char *argv[]){
+        /* init the dialog API */
+        init_dialog(stdin, stdout);
 
 
 	return 0;
