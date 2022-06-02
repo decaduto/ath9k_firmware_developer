c7191d1a7699219d7eeb8e4ff51c07d6bea32768
decaduto <78024779+decaduto@users.noreply.github.com>
2022-06-03 00:14:03 +0200

    fixed in memory section layout, now .firmware is marked as rodata and is loaded in memory at program runtime.
      Tipo           Offset   IndirVirt          IndirFis           DimFile  DimMem   Flg Allin
      PHDR           0x000040 0x0000000000000040 0x0000000000000040 0x000310 0x000310 R   0x8
      INTERP         0x000350 0x0000000000000350 0x0000000000000350 0x00001c 0x00001c R   0x1
          [Requesting program interpreter: /lib64/ld-linux-x86-64.so.2]
      LOAD           0x000000 0x0000000000000000 0x0000000000000000 0x000d68 0x000d68 R   0x1000
      LOAD           0x001000 0x0000000000001000 0x0000000000001000 0x000639 0x000639 R E 0x1000
      LOAD           0x002000 0x0000000000002000 0x0000000000002000 0x00c99c 0x00c99c R   0x1000
      LOAD           0x00eda8 0x000000000000fda8 0x000000000000fda8 0x0005fe 0x000610 RWE 0x1000
      DYNAMIC        0x00edc8 0x000000000000fdc8 0x000000000000fdc8 0x000210 0x000210 RW  0x8
      NOTE           0x000370 0x0000000000000370 0x0000000000000370 0x000040 0x000040 R   0x8
      NOTE           0x0003b0 0x00000000000003b0 0x00000000000003b0 0x000044 0x000044 R   0x4
      TLS            0x00eda8 0x000000000000fda8 0x000000000000fda8 0x000000 0x0000c8 R   0x8
      GNU_PROPERTY   0x000370 0x0000000000000370 0x0000000000000370 0x000040 0x000040 R   0x8
      GNU_EH_FRAME   0x00e76c 0x000000000000e76c 0x000000000000e76c 0x000074 0x000074 R   0x4
      GNU_STACK      0x000000 0x0000000000000000 0x0000000000000000 0x000000 0x000000 RW  0x10
      GNU_RELRO      0x00eda8 0x000000000000fda8 0x000000000000fda8 0x000258 0x000258 R   0x1
    
     Mappatura da sezione a segmento:
      Sezioni del segmento...
       00     
       01     .interp 
       02     .interp .note.gnu.property .note.gnu.build-id .note.ABI-tag .gnu.hash .dynsym .dynstr .gnu.version .gnu.version_r .rela.dyn .rela.plt 
       03     .init .plt .text .fini 
       04     .rodata .firmware .eh_frame_hdr .eh_frame 
       05     .init_array .fini_array .dynamic .got .got.plt .data .ath9u_ops .boot .bss 
       06     .dynamic 
       07     .note.gnu.property 
       08     .note.gnu.build-id .note.ABI-tag 
       09     .tbss 
       10     .note.gnu.property 
       11     .eh_frame_hdr 
       12     
       13     .init_array .fini_array .dynamic .got

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 gcc -c pprTTxv.c
-objcopy --add-section .firmware=ath9u_fw/htc_9271.fw --set-section-flags .firmware=load,readonly pprTTxv.o final.o
+objcopy --add-section .firmware=ath9u_fw/htc_9271.fw --set-section-flags .firmware=alloc,readonly pprTTxv.o final.o
 gcc -ldialog -lncurses -lusb-1.0  final.o -o ath9u
 rm *.o
